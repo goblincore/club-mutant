a9bc24d008c7106fb7ad81dfca9218e450c2b232
Donny <donny@Donnys-MacBook-Air.local>
2026-02-04 14:34:34 -0500

    Add missing ElectronAPI properties

--- client/src/types/electron.d.ts
+++ client/src/types/electron.d.ts
@@ -1,7 +1,13 @@
 interface ElectronAPI {
+  isElectron: boolean
   saveFile: (data: string, filename: string) => Promise<string | null>
   readFile: () => Promise<{ data: string; filename: string } | null>
   getAppVersion: () => Promise<string>
+  saveFileDialog: (options: {
+    content: string
+    defaultPath: string
+  }) => Promise<{ canceled: boolean; filePath?: string }>
+  openFileDialog: () => Promise<{ canceled: boolean; filePaths?: string[] }>
 }
 
 interface Window {
